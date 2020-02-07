373df065f0b4416823463454885fa7bfdd3d364f
JLM <jianglimin999@gmail.com>
2020-02-07 17:15:03 +0800

    完成正常登录

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,18 +1,22 @@
 const Router = require('koa-router')
-const router = new Router()
+const router = new Router({
+    prefix: '/v1/classic'
+})
+const {Auth} = require('../../../middlewares/auth')
 
 const {PositiveIntegerValidator} = require('../../validators/validator')
 
-router.post('/v1/:id/classic/latest', (ctx, next) => {
-    const param = ctx.params
-    const query = ctx.request.query
-    const body = ctx.request.body
-    const header = ctx.request.headers
+router.get('/latest', new Auth().m,(ctx, next) => {
+    // const param = ctx.params
+    // const query = ctx.request.query
+    // const body = ctx.request.body
+    // const header = ctx.request.headers
 
-    const v = new PositiveIntegerValidator().validate(ctx)
-    ctx.body = {
-        key: 'latest'
-    }
+    // const v = new PositiveIntegerValidator().validate(ctx)
+    // ctx.body = {
+    //     key: 'latest'
+    // }
+    ctx.body = ctx.auth.uid
 })
 
 module.exports = router
