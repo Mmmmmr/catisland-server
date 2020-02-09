aeba1640c3e21187d16e970588ed084f9f5e0b69
JLM <jianglimin999@gmail.com>
2020-02-10 00:18:17 +0800

    '完成classic接口'

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,30 +1,118 @@
 const Router = require('koa-router')
+const {
+    Flow
+} = require('../../model/flow')
+const {
+    Favor
+} = require('../../model/favor')
+const {
+    PositiveIntegerValidator,
+    ClassicValidator
+} = require('../../validators/validator')
+
 const router = new Router({
     prefix: '/v1/classic'
 })
-const {Auth} = require('../../../middlewares/auth')
-const {Flow} = require('../../model/flow')
+// 别名 alias module-alias
+const {
+    Auth
+} = require('../../../middlewares/auth')
 
-const {Art} = require('../../model/art')
+const {
+    Art
+} = require('../../model/art')
 
-const {PositiveIntegerValidator} = require('../../validators/validator')
 
-router.get('/latest', new Auth().m,async (ctx, next) => {
-    // const param = ctx.params
-    // const query = ctx.request.query
-    // const body = ctx.request.body
-    // const header = ctx.request.headers
+router.get('/latest', new Auth().m, async (ctx, next) => {
+
     const flow = await Flow.findOne({
         order: [
-            [
-                'index', 'DESC'
-            ]
+            ['index', 'DESC']
         ]
     })
     const art = await Art.getData(flow.art_id, flow.type)
+    const i = art.get('image')
+    const t = art.image
+    const s = art.getDataValue('image')
+    const likeLatest = await Favor.userLikeIt(
+        flow.art_id, flow.type, ctx.auth.uid)
     art.setDataValue('index', flow.index)
+    art.setDataValue('like_status', likeLatest)
     ctx.body = art
 })
 
-module.exports = router
+router.get('/:index/next', new Auth().m, async (ctx) => {
+    const v = await new PositiveIntegerValidator().validate(ctx, {
+        id: 'index'
+    })
+    const index = v.get('path.index')
+    const flow = await Flow.findOne({
+        where: {
+            index: index + 1
+        }
+    })
+    if (!flow) {
+        throw new global.errs.NotFound()
+    }
+    const art = await Art.getData(flow.art_id, flow.type)
+    const likeNext = await Favor.userLikeIt(
+        flow.art_id, flow.type, ctx.auth.uid)
+    art.setDataValue('index', flow.index)
+    art.setDataValue('like_status', likeNext)
+    // art.exclude = ['index','like_status']
+    ctx.body = art
+})
 
+router.get('/:index/previous', new Auth().m, async (ctx) => {
+    const v = await new PositiveIntegerValidator().validate(ctx, {
+        id: 'index'
+    })
+    const index = v.get('path.index')
+    const flow = await Flow.findOne({
+        where: {
+            index: index - 1
+        }
+    })
+    if (!flow) {
+        throw new global.errs.NotFound()
+    }
+    const art = await Art.getData(flow.art_id, flow.type)
+    const likePrevious = await Favor.userLikeIt(
+        flow.art_id, flow.type, ctx.auth.uid)
+    art.setDataValue('index', flow.index)
+    art.setDataValue('like_status', likePrevious)
+    ctx.body = art
+})
+
+
+router.get('/:type/:id', new Auth().m, async ctx=>{
+    const v = await new ClassicValidator().validate(ctx)
+    const id = v.get('path.id')
+    const type = parseInt(v.get('path.type'))
+
+    const artDetail =await new Art(id,type).getDetail(ctx.auth.uid)
+
+    artDetail.art.setDataValue('like_status', artDetail.like_status)
+    ctx.body = artDetail.art
+})
+
+router.get('/:type/:id/favor', new Auth().m, async ctx => {
+    const v = await new ClassicValidator().validate(ctx)
+    const id = v.get('path.id')
+    const type = parseInt(v.get('path.type'))
+
+    const artDetail =await new Art(id,type).getDetail(ctx.auth.uid)
+
+    ctx.body = {
+        fav_nums: artDetail.art.fav_nums,
+        like_status: artDetail.like_status
+    }
+})
+
+router.get('/favor', new Auth().m, async ctx => {
+    const uid = ctx.auth.uid
+    ctx.body = await Favor.getMyClassicFavors(uid)
+})
+
+
+module.exports = router
--- app/model/favor.js
+++ app/model/favor.js
@@ -1,19 +1,26 @@
-const {sequelize} = require('../../core/db')
+const {
+    sequelize
+} = require('../../core/db')
 const {
     Sequelize,
-    Model
+    Model,
+    Op
 } = require('sequelize')
+const {
+    Art
+} = require('./art')
 
-const {Art} = require('./art')
-
-class Favor extends Model{
-    static async like(art_id, type, uid){
-        const favor =await Favor.findOne({
-            art_id,
-            type,
-            uid
+class Favor extends Model {
+    // 业务表
+    static async like(art_id, type, uid) {
+        const favor = await Favor.findOne({
+            where: {
+                art_id,
+                type,
+                uid
+            }
         })
-        if(favor){
+        if (favor) {
             throw new global.errs.LikeError()
         }
         return sequelize.transaction(async t => {
@@ -21,36 +28,92 @@ class Favor extends Model{
                 art_id,
                 type,
                 uid
-            }, {transaction: t})
-            const art = await Art.getData(art_id, type)
-            await art.increment('fav_nums', {by: 1, transaction: t})
+            }, {
+                transaction: t
+            })
+            const art = await Art.getData(art_id, type, false)
+            await art.increment('fav_nums', {
+                by: 1,
+                transaction: t
+            })
         })
     }
 
-    static async dislike(art_id, type, uid){
-        const favor =await Favor.findOne({
-            art_id,
-            type,
-            uid
+    static async disLike(art_id, type, uid) {
+        const favor = await Favor.findOne({
+            where: {
+                art_id,
+                type,
+                uid
+            }
         })
-        if(!favor){
+        if (!favor) {
             throw new global.errs.DislikeError()
         }
+        // Favor 表 favor 记录
         return sequelize.transaction(async t => {
             await favor.destroy({
                 force: true,
                 transaction: t
             })
-            const art = await Art.getData(art_id, type)
-            await art.decrement('fav_nums', {by: 1, transaction: t})
+            const art = await Art.getData(art_id, type, false)
+            await art.decrement('fav_nums', {
+                by: 1,
+                transaction: t
+            })
         })
+    }
 
+    static async userLikeIt(art_id, type, uid) {
+        const favor = await Favor.findOne({
+            where: {
+                uid,
+                art_id,
+                type,
+            }
+        })
+        return favor ? true : false
+    }
+
+    static async getMyClassicFavors(uid) {
+        const arts = await Favor.findAll({
+            where: {
+                uid,
+                type:{
+                    [Op.not]:400,
+                }
+            }
+        })
+        if(!arts){
+            throw new global.errs.NotFound()
+        }
+
+        return await Art.getList(arts)
+    }
+
+    static async getBookFavor(uid, bookID){
+        const favorNums = await Favor.count({
+            where: {
+                art_id:bookID,
+                type:400
+            }
+        })
+        const myFavor = await Favor.findOne({
+            where:{
+                art_id:bookID,
+                uid,
+                type:400
+            }
+        })
+        return {
+            fav_nums:favorNums,
+            like_status:myFavor?1:0
+        }
     }
 }
 
-
 Favor.init({
-    uid: Sequelize.STRING,
+    uid: Sequelize.INTEGER,
     art_id: Sequelize.INTEGER,
     type: Sequelize.INTEGER
 }, {
@@ -61,3 +124,4 @@ Favor.init({
 module.exports = {
     Favor
 }
+
