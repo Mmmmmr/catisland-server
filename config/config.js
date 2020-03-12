e42802c1d642e96af1455ceba2286c5e02d257be
JLM <jianglimin999@gmail.com>
2020-03-12 13:15:33 +0800

    df

--- config/config.js
+++ config/config.js
@@ -1,26 +1,27 @@
 module.exports = {
-    environment: 'prod',
-    database: {
-        dbName:'catisland',
-        host:'ciliuciliu.club',
-        port:3306,
-        user:'root',
-        password:'root',
-    },
-    security: {
-        secretKey:  "abcdefg",
-        expiresIn:  60*60*24*30
-        // expiresIn:  10
-
-    },
-    wx: {
-        appId: 'wx5173ed31af8ed115',
-        appSecret: '55a06e76e1ad2fe058cffd21e54421f2',
-        loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
-    },
-    yushu:{
-        detailUrl:'http://t.yushu.im/v2/book/id/%s',
-        keywordUrl:'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
-    },
-    host:'http://catisland.ciliuciliu.club/'
-}
+  environment: "prod",
+  database: {
+    dbName: "catisland",
+    host: "ciliuciliu.club",
+    port: 3306,
+    user: "root",
+    password: "root"
+  },
+  security: {
+    secretKey: "abcdefg",
+    expiresIn: 60 * 60 * 24 * 30
+    // expiresIn:  10
+  },
+  wx: {
+    appId: "wx5173ed31af8ed115",
+    appSecret: "55a06e76e1ad2fe058cffd21e54421f2",
+    loginUrl:
+      "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
+  },
+  yushu: {
+    detailUrl: "http://t.yushu.im/v2/book/id/%s",
+    keywordUrl:
+      "http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s"
+  },
+  host: "https://catisland.ciliuciliu.club/"
+};
